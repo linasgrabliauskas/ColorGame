7e9dc6ccf60277aa6e67668b14054fbb825dd71b
Linas <linas.grabliauskas@gmail.com>
2021-01-24 18:10:12 +0200

    First commit

---
+++ scripts/script.js
@@ -0,0 +1,201 @@
+// Global variables
+let startGameBtn = document.querySelector("#start-game-btn")
+let colorsBoard = document.querySelector('.game-board__colors')
+let mainColorBlock = document.querySelector(".game-board__main-color")
+let scoreAmount = document.querySelector("#score-amount")
+let timeLeftOutput = document.querySelector("#time-amount")
+let levelBtns = document.querySelectorAll('.level')
+let levelTitle = document.querySelector('.game-level-headline')
+let boxes = document.querySelectorAll(".box")
+let headline = document.querySelector('.game-level-headline')
+let description = document.querySelector('.game-level-description')
+let headlineHome = document.querySelector('.headline-home')
+let homeBtn = document.querySelector('.home')
+let scoreboardBtn = document.querySelector('.scoreboard')
+// Section blocks
+let headlineBlock = document.querySelector('.headline-section')
+let gameBlock = document.querySelector('.game-section')
+let scoreBlock = document.querySelector('.score-section')
+let scoreboardBlock = document.querySelector('.scoreboard-section')
+let scoreTable = document.querySelector('.table-body')
+// Score and time
+let score = 0
+let time = 15000
+let timeUp = true
+let fullTimer, timer
+
+// Functions
+function setColor() {
+    boxes = document.querySelectorAll(".box")
+    boxes.forEach(box => box.addEventListener("click", pickColor))
+    boxes.forEach(box => {
+        let h = Math.floor(Math.random()* 359)
+        let s = Math.floor(Math.random()* 359) + '%'
+        let l = Math.floor(Math.random()* 50) + '%'
+        box.style.backgroundColor = `hsl(${h},${s},${l})`
+    })
+    // Mini Bug fix
+    if (boxes.length === 0) return
+    let i = Math.floor(Math.random() * boxes.length)
+    mainColorBlock.style.backgroundColor = boxes[i].style.backgroundColor
+}
+
+function startGame() {
+    // Reset blocks
+    headline.style.display = 'none'
+    description.style.display = 'none'
+    startGameBtn.style.display = 'none'
+    scoreBlock.style.display = 'block'
+    mainColorBlock.style.display = 'flex'
+    // Clear intervals
+    clearTimeout(fullTimer)
+    clearInterval(timer)
+    // Reset stats
+    timeUp = false
+    let timeLeft = time / 1000
+    score = 0
+    // Setup boards
+    setColor()
+    // Set timers
+    fullTimer = setTimeout(() => {
+        timeUp = true
+        addHighscore()
+    }, time)
+    timer = setInterval(() => timeLeftOutput.innerText = timeLeft > 0 ? `${--timeLeft} seconds` : `Times up!`, 1000)
+}
+
+function pickColor(e) {
+    if(!timeUp) {
+        let currentBoxColor = getComputedStyle(e.target).backgroundColor
+        let mainBoxColor = getComputedStyle(mainColorBlock).backgroundColor
+        if(currentBoxColor === mainBoxColor) {
+            score++
+            scoreAmount.innerText = score
+            setColor()
+        }
+    }
+}
+
+function setLevel(e){
+    score = 0
+    scoreAmount.innerText = score
+    // Reset blocks
+    headlineBlock.style.display = 'block'
+    headline.style.display = 'block'
+    description.style.display = 'block'
+    startGameBtn.style.display = 'block'
+    gameBlock.style.display = 'block'
+    mainColorBlock.style.display = 'none'
+    scoreBlock.style.display = 'none'
+    headlineHome.style.display = 'none'
+    scoreboardBlock.style.display = 'none'
+    // Clear timers if game already running
+    clearTimeout(fullTimer)
+    clearInterval(timer)
+    // Create colors board on level
+    time = +e.target.dataset.time
+    let boardBoxes = ''
+    // Create boxes
+    let boxCount = +e.target.dataset.level
+    for (let i=0; i < boxCount; i++){
+        boardBoxes += '<div class="box"></div>'
+    }
+    colorsBoard.innerHTML = boardBoxes
+    timeLeftOutput.innerText = `${time / 1000} seconds`
+    setColor()
+    if (boxCount === 9) boxes.forEach(box => {
+        box.style.width = 'calc(33.33% - 10px)'
+        box.style.height = `4em`
+        levelTitle.innerText = 'EASY LEVEL'
+    })
+    if (boxCount === 16) boxes.forEach(box => {
+        box.style.width = 'calc(25% - 10px)'
+        box.style.height =  `3em`
+        levelTitle.innerText = 'MEDIUM LEVEL'
+    })
+    if (boxCount === 25) boxes.forEach(box => {
+        box.style.width = 'calc(20% - 10px)'
+        box.style.height = `2.5em`
+        levelTitle.innerText = 'HARD LEVEL'
+    })
+}
+
+function setHomePage(){
+    headline.style.display = 'none'
+    description.style.display = 'none'
+    startGameBtn.style.display = 'none'
+    scoreBlock.style.display = 'none'
+    scoreboardBlock.style.display = 'none'
+    headlineBlock.style.display = 'block'
+    gameBlock.style.display = 'block'
+    headlineHome.style.display = 'block'
+    mainColorBlock.style.display = 'flex'
+    setColor()
+}
+
+function setScoreboardPage(){
+    // Reset blocks
+    scoreBlock.style.display = 'none'
+    headlineBlock.style.display = 'none'
+    gameBlock.style.display = 'none'
+    scoreboardBlock.style.display = 'block'
+    let scoresArr
+    // Take data from local storage
+    if (localStorage.hasOwnProperty('findColorGame')){
+        let scoresJSON = localStorage.getItem('findColorGame')
+        scoresArr = JSON.parse(scoresJSON)
+    } else {
+        return
+    }
+    // Filter scores without names or score = 0
+    let scoreData = scoresArr.filter(score => score.nick && score.score)
+    // Filter scores data by score
+    let sortedScores = scoreData.sort((a,b) => b.score - a.score)
+    let scoresElements = ''
+    sortedScores.forEach((score, index) => {
+        scoresElements += `
+           <tr>
+               <td class="score-place">${index +1}</td>
+               <td class="score-player">${score.nick}</td>
+               <td class="score-score">${score.score}</td>
+           </tr>
+        `
+    })
+    scoreTable.innerHTML = scoresElements
+}
+
+
+function addHighscore(){
+    // Input name
+    let name = prompt("Your name or nickname:")
+    // Get the existing data
+    let scoresArr
+    if (localStorage.hasOwnProperty('findColorGame')){
+        let scoresJSON = localStorage.getItem('findColorGame')
+        scoresArr = JSON.parse(scoresJSON)
+    } else {
+        scoresArr = []
+    }
+    if(name){
+        scoresArr.push({ nick: name, score })
+        // Save back to localStorage
+        localStorage.setItem('findColorGame', JSON.stringify(scoresArr))
+    }
+    // Reset Score 
+    score = 0
+    setScoreboardPage()
+}
+
+// Events
+// - Load home page onLoad and on HomeClick
+document.addEventListener("DOMContentLoaded", setHomePage)
+homeBtn.addEventListener('click', setHomePage)
+startGameBtn.addEventListener("click", startGame)
+boxes.forEach(box => box.addEventListener("click", pickColor))
+levelBtns.forEach(btn => btn.addEventListener('click', setLevel))
+scoreboardBtn.addEventListener('click', setScoreboardPage)
+
+
+
+
+
